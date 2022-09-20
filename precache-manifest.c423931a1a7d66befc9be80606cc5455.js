2b7f0e3c24c1adcb44d07fbf89fcec9a675382da
Jose Carlos Colque Montaño <montanoj47@gmail.com>
2022-09-20 10:51:46 -0400

    Updates

---
+++ precache-manifest.c423931a1a7d66befc9be80606cc5455.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62c6333214cf295b3c5173891ac53d8",
+    "url": "/JoseeCarlos/project-solnaciente.git/index.html"
+  },
+  {
+    "revision": "5db6936abbf6aa7680e8",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/css/2.ed5b23ac.chunk.css"
+  },
+  {
+    "revision": "6cd3908b86dc4c0e2a68",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/css/main.b705a980.chunk.css"
+  },
+  {
+    "revision": "5db6936abbf6aa7680e8",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/js/2.259f709d.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/js/2.259f709d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645ef9baf9a9983e7e55",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/js/3.45710c49.chunk.js"
+  },
+  {
+    "revision": "8076a39f2c6eb4b606ff15bd0342709c",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/js/3.45710c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd3908b86dc4c0e2a68",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/js/main.e16ad5de.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/js/main.e16ad5de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633f563da1c21b181059",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/js/runtime-main.53d8c517.js"
+  },
+  {
+    "revision": "73e6b012bf1e5ab3ba41033aa8f977de",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/media/flags_responsive.73e6b012.png"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/JoseeCarlos/project-solnaciente.git/static/media/primeicons.e01fd413.eot"
+  }
+]);
